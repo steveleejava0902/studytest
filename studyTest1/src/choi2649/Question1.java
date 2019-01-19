a62ea57218ecd65ae5bb38b585384122ad9121d2
apple <apple@218.38.137.28>
2019-01-19 14:37:55 +0900

    1번 문

---
+++ studyTest1/src/choi2649/Question1.java
@@ -0,0 +1,13 @@
+package choi2649;
+
+public class Question1 {
+	public static void main(String[] args) {
+		//System.out.println("test");
+		Question1 q = new Question1();
+		q.printTest("스티브리자바테스트문제풀이중");
+	}
+
+	private void printTest(String string) {
+		System.out.println(string);
+	}
+}
