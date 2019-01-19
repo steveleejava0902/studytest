08635cf29aaefcacb16c706238910bdcb233660c
fjsej <fjsej@DESKTOP-76SKU6T>
2019-01-19 14:26:13 +0900

    1번 문제 품

---
+++ studyTest1/src/fjsejs128/Question1.java
@@ -0,0 +1,20 @@
+package fjsejs128;
+
+public class Question1 {
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Question1 q = new Question1();
+		q.printTest("스티브리자바테스트문제풀이중");
+		
+	}
+
+	private void printTest(String string) {
+		// TODO Auto-generated method stub
+		System.out.println(string);
+	}
+
+}
