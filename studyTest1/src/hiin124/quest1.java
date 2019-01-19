c3c9d8dcd11264c86836a547fc193b1a9869aa87
Kim youngwoo <38499381+Andrewkim92@users.noreply.github.com>
2019-01-19 17:28:31 +0900

    quest1

---
+++ studyTest1/src/hiin124/quest1.java
@@ -0,0 +1,14 @@
+package hiin124;
+
+public class quest1 {
+
+	public static void main(String[] args) {
+		String temp = "스티브리자바테스트문제풀이중";
+		printTest(temp);
+	}
+
+	private void printTest(String string) {
+		// TODO Auto-generated method stub
+		System.out.println(string);
+	}
+}
