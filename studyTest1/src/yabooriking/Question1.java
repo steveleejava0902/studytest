206e2c150db53d991f7c5f6054d20596b084492e
Kim jae ho <kjh@itcommerce.co.kr>
2019-01-19 17:31:26 +0900

    .

---
+++ studyTest1/src/yabooriking/Question1.java
@@ -0,0 +1,16 @@
+package yabooriking;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		
+		Question1 question = new Question1();
+		question.printTest("스티브리자바테스트문제풀이중");
+	}
+
+	private void printTest(String string) {
+		System.out.println(string);
+		
+	}
+
+}
