724b0576a4fe7e97013ccdebaf3b58162eebc252
Kim youngwoo <38499381+Andrewkim92@users.noreply.github.com>
2019-01-19 17:31:52 +0900

    quest2 commit

---
+++ studyTest1/src/hiin124/quest2.java
@@ -0,0 +1,16 @@
+package hiin124;
+
+public class quest2 {
+
+	
+	public static void main(String[] args) {
+		String val = new String("value");
+		
+		if(val.equals("value")) {
+			System.out.println("동작했음. 잘했어");
+		}else {
+			System.out.println("동작못했어. 다시 생각해봐");
+		}
+		
+	}
+}
