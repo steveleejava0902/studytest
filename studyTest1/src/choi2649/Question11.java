b1b859d197588f9c4030c28f6e17c8e181c40cab
apple <apple@218.38.137.28>
2019-01-26 07:55:11 +0900

    커밋테스

--- studyTest1/src/choi2649/Question11.java
+++ studyTest1/src/choi2649/Question11.java
@@ -4,11 +4,11 @@ public class Question11 {
 
 	public static void main(String[] args) {
 		String val = new String("value");
-
-		if("value".equals(val)) {
-		System.out.println("동작했음. 잘했어");
+		val = "value";
+		if("value"==val) {
+			System.out.println("동작했음. 잘했어");
 		}else {
-		System.out.println("동작못했어. 다시 생각해봐");
+			System.out.println("동작못했어. 다시 생각해봐");
 		}
 	}
 
