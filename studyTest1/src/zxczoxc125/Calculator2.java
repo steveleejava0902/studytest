e65d28b75c51bcca982e52f5afba649d7f15f113
이재훈 <zxczoxc125@gmail.com>
2019-01-26 16:32:02 +0900

    Calculator 3 구현

--- studyTest1/src/zxczoxc125/Calculator2.java
+++ studyTest1/src/zxczoxc125/Calculator2.java
@@ -1,9 +1,6 @@
 package zxczoxc125;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Graphics;
-import java.awt.Insets;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -11,36 +8,35 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
 
 public class Calculator2 extends JFrame implements KeyListener {
 	JPanel mainPanel;
 	JTextArea inputArea;
 	JLabel explain;
-	
+
 	public Calculator2() {
 		mainPanel = new JPanel();
 		mainPanel.setBorder(new TitledBorder("panel"));
 		mainPanel.setLayout(new BorderLayout());
-		
+
 		setContentPane(mainPanel);
-		
+
 		inputArea = new JTextArea("0");
-		
+
 		explain = new JLabel("설명창입니다.", JLabel.RIGHT);
-		
+
 		mainPanel.add(inputArea, BorderLayout.NORTH);
 		mainPanel.add(explain, BorderLayout.CENTER);
-		
+
 		inputArea.addKeyListener(this);
-		
+
 		setTitle("Calculator");
 		setVisible(true);
 		setSize(300, 200);
 		setLocationRelativeTo(null);
 	}
-	
+
 	public static void main(String[] args) {
 		Calculator2 calculator2 = new Calculator2();
 	}
@@ -48,19 +44,25 @@ public class Calculator2 extends JFrame implements KeyListener {
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
+		char key = e.getKeyChar();
+
+		if (key == 10) {
+			explain.setText(inputArea.getText());
+			inputArea.setText("");
+		}
 	}
 
 	@Override
-	public void keyPressed(KeyEvent e) {
+	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		System.out.println(111);
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 }
