f597604c6b7f13b9f33e9cd0fd5f3a4ff979c7e9
dongY <ldcpaul@hanmail.net>
2019-02-09 13:39:30 +0900

    Q1

---
+++ studyTest1/src/ldcpaul/Question1.java
@@ -0,0 +1,19 @@
+package ldcpaul;
+
+public class Question1 {
+
+	/*
+	 * 1 자신의 패키지 밑에 Question1 클래스파일을 생성한다
+2 main 메서드 안에 printTest("스티브리자바테스트문제풀이중"); 이라고 코딩한다.
+3 에러가나는 곳에 마우스를 올리고 create method를 클릭하여 자동으로 메서드를 생성한다.
+4 메서드 안에 System.out.println(string); 를 코딩하고 실행해본다
+5 출력을 확인한 후 메서드명 앞에 있는 static을 제거한다.
+6 에러가 나는 상태에서 main메서드 안쪽에서만 코딩을 해서 printTest메서드를 실행해서 첫 실행과 같은 결과를 실행해봐라.
+7 성공하면 소스를 commit and push하고 다음문제로 넘어간다*/
+	public static void main(String [] arg) {
+		printTest("스티브리자바테스트문제풀이중");
+	}
+	public static void printTest(String string) {
+		System.out.println(string);
+	}
+}
