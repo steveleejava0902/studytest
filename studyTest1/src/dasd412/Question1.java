3ee3e88c9d3ca5dd0f55b6e7ec4557cc920c65e0
0 <0@DESKTOP-J70PGIJ>
2019-02-09 13:12:06 +0900

    questin01

---
+++ studyTest1/src/dasd412/Question1.java
@@ -0,0 +1,13 @@
+package dasd412;
+
+public class Question1 {
+public static void main(String[] args) {
+	Question1 question=new Question1();
+	question.printTest("스티브리자바테스트문제풀이중");
+}
+
+private  void printTest(String string) {
+	// TODO Auto-generated method stub
+	System.out.println(string);
+}
+}
